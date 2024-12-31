205c4f63dab13595537d210e4322819ebbb2dd5a
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2024-12-31 09:33:33 +0000

    Day12 part 2

--- day12/index.mjs
+++ day12/index.mjs
@@ -74,7 +74,7 @@ const mergeGroupOfRegions = (groupOfRegions) => {
         if (shouldBeSingleRegion(r1, r2)) {
           const mergedRegion = { plant: r1.plant, locations: r1.locations.concat(r2.locations) };
           const otherRegions = workingRegions.filter((r) => r !== r1 && r !== r2);
-          workingRegions =[mergedRegion, ...otherRegions];
+          workingRegions = [mergedRegion, ...otherRegions];
           somethingMerged = true;
         }
       }
@@ -127,6 +127,66 @@ const calculatePerimeter = (region) => {
   return perimeter;
 };
 
+const diffsBy = (xs, fn) => range(xs.length - 1).map((index) => fn(xs[index + 1]) - fn(xs[index]));
+
+const countConsecutiveLines = (xs, compareFn, groupByFn, diffByFn) => {
+  const sorted = xs.slice().sort(compareFn);
+  const grouped = groupBy(sorted, groupByFn);
+  const lists = Array.from(grouped.values());
+  const diffs = lists.flatMap((list) => diffsBy(list, diffByFn));
+  return diffs.filter((diff) => diff === 1).length;
+};
+
+const countConsecutiveHorizontalLines = (xs) => {
+  const compareFn = (a, b) => {
+    return a.row === b.row ? a.col - b.col : a.row - b.row;
+  };
+
+  const groupByFn = (x) => `${x.row}:${x.dir}`;
+  const diffByFn = (x) => x.col;
+
+  return countConsecutiveLines(xs, compareFn, groupByFn, diffByFn);
+};
+
+const countConsecutiveVerticalLines = (xs) => {
+  const compareFn = (a, b) => {
+    return a.col === b.col ? a.row - b.row : a.col - b.col;
+  };
+
+  const groupByFn = (x) => `${x.col}:${x.dir}`;
+  const diffByFn = (x) => x.row;
+
+  return countConsecutiveLines(xs, compareFn, groupByFn, diffByFn);
+};
+
+const calculatePerimeter2 = (region) => {
+  const rows = region.length;
+  const cols = region[0].length;
+
+  const xs = [];
+
+  for (const row of range(rows)) {
+    for (const col of range(cols)) {
+      if (region[row][col] === ".") continue;
+      const ns = [
+        { row: row - 1, col, dir: 0 }, // dir is facing up
+        { row: row + 1, col, dir: 1 }, // dir is facing down
+        { row, col: col - 1, dir: 2 }, // dir is facing left
+        { row, col: col + 1, dir: 3 }, // dir is facing right
+      ].filter(({ row, col }) => {
+        const ch = region[row]?.[col] ?? ".";
+        return ch === ".";
+      });
+      xs.push(...ns);
+    }
+  }
+
+  const a = countConsecutiveHorizontalLines(xs);
+  const b = countConsecutiveVerticalLines(xs);
+
+  return xs.length - a - b;
+};
+
 const makeRegionGrid = (region) => {
   const allRowValues = region.locations.map(({ row }) => row);
   const allColValues = region.locations.map(({ col }) => col);
@@ -148,10 +208,12 @@ const makeRegionGrid = (region) => {
   return regionGrid;
 };
 
-const calculatePrice = (region) => {
+const calculatePrice = (region, part2Mode) => {
   const regionGrid = makeRegionGrid(region);
   const area = calculateArea(regionGrid);
-  const perimeter = calculatePerimeter(regionGrid);
+  const perimeter = part2Mode
+    ? calculatePerimeter2(regionGrid)
+    : calculatePerimeter(regionGrid);
   console.log({ plant: region.plant, area, perimeter, price: area * perimeter });
   return area * perimeter;
 };
@@ -160,7 +222,16 @@ const part1 = async (filename) => {
   const grid = await readGrid(filename);
   const regions = findRegions(grid);
   const mergedRegions = mergeRegions(regions);
-  const prices = mergedRegions.map(calculatePrice);
+  const prices = mergedRegions.map((region) => calculatePrice(region, false));
+  const total = sum(prices);
+  console.log(total);
+};
+
+const part2 = async (filename) => {
+  const grid = await readGrid(filename);
+  const regions = findRegions(grid);
+  const mergedRegions = mergeRegions(regions);
+  const prices = mergedRegions.map((region) => calculatePrice(region, true));
   const total = sum(prices);
   console.log(total);
 };
@@ -168,6 +239,13 @@ const part1 = async (filename) => {
 const main = async () => {
   await part1("day12/example.txt");
   await part1("day12/input.txt");
+
+  await part2("day12/part2-a.txt");
+  await part2("day12/part2-b.txt");
+  await part2("day12/part2-c.txt");
+  await part2("day12/part2-d.txt");
+  await part2("day12/example.txt");
+  await part2("day12/input.txt");
 };
 
 main();
